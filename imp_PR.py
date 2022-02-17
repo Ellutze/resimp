92fc71545f9153d67bd9281a6d856d159213302b
Jakub Kucera <Jakub.Kucera@nccuk.com>
2022-02-17 15:57:37 +0000

    txt files prep

---
+++ imp_PR.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 14:54:15 2022
+
+@author: jakub.kucera
+"""
+
+#open input file that checks the file it needs to reach - number of tables, name of tables....
---
+++ resimp_main.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 14:50:17 2022
+
+@author: jakub.kucera
+"""
+
+#Run GUI
+
+
+
+#layout:
+# import file field, PAM-RTM install location, initiate button
+
+
+
+
+# upon button press:
+
+#translate any input file to importable .txt files 
+
+#save file with meta-data  
+
+#initiate command line 
+
+
+#after import is done make sure to delete everything in "tt" (temporary temperature tables)
